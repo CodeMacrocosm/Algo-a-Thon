e18145e4eec1934aa60daca75b0716d4b393d5ed
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-10-06 20:23:05 +0530

    Create Binary_search.java

---
+++ Java/Binary_search.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 18;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+    // return the index
+    // return -1 if it does not exist
+    static int binarySearch(int[] arr, int target){
+       int start = 0;
+       int end = arr.length - 1;
+
+       while(start <= end){
+           // find the middle element
+//           int mid = (start + end) / 2; // might be possible that (start+end) exceeds the range of int in Java
+            int mid = start + (end - start) / 2; // same as above but better way
+
+           if(target < arr[mid]){
+               end = mid - 1;
+           } else if(target > arr[mid]){
+               start = mid + 1;
+           } else {
+               // ans found
+               return mid;
+           }
+       }
+        return -1;
+    }
+}
