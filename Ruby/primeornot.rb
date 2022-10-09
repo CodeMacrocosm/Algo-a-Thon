0339d8bb0685ed393441977569e48484c2fbdf48
Pratyusha-spec <78694085+Pratyusha-spec@users.noreply.github.com>
2022-10-09 14:51:58 +0530

    Create primeornot.rb

---
+++ Ruby/primeornot.rb
@@ -0,0 +1,21 @@
+puts "Enter the number:"
+num=gets.chomp.to_i
+count=0
+if (num==0)
+	puts "0 is not prime"
+else
+	
+	i=2
+	while(i<num)
+		if (num%i==0)
+			count+=1
+		end
+		i+=1
+	end
+	
+end
+	if count>1
+		puts "#{num} is not a prime number"
+	else
+		puts "#{num} is a prime number"
+	end
