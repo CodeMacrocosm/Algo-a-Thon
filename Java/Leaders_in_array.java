7d4863b3a5aff76e6191fce24ee296a394c8e44a
Vedant0 <68452337+Vedant0@users.noreply.github.com>
2022-10-17 20:33:18 +0530

    Create Leaders_in_array.java

---
+++ Java/Leaders_in_array.java
@@ -0,0 +1,29 @@
+package com.company;
+
+class Main
+{
+
+    void printLeaders(int arr[], int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int j;
+            for (j = i + 1; j < size; j++)
+            {
+                if (arr[i] <=arr[j])
+                    break;
+            }
+            if (j == size)
+                System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        Main lead = new Main();
+        int arr[] = new int[]{16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+        lead.printLeaders(arr, n);
+    }
+}
