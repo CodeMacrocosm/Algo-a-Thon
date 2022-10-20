8765a175973a5afc936d823417c64dd2b5060fcb
Rohit Gupta <wondermyhome25@gmail.com>
2022-10-21 03:50:22 +0530

    Create applicationbilling.py

---
+++ Python/applicationbilling.py
@@ -0,0 +1,6 @@
+//write a python program to create a application to read no of pizza, no of burger, no of cooldrinks and print the final billing
+pizza=int(input("enter the no of pizza"))
+burger=int(input("enter the no of burger"))
+cd=int(input("enter the no of cd"))
+total=pizza*100+burger*60+cd*20
+print("total=",total)
