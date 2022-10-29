1268fa4098b9e61fff10dd04b13f584fc250a7a4
gupta-ansh <115942426+gupta-ansh@users.noreply.github.com>
2022-10-30 00:02:33 +0530

    Add floyd_warshall.py
    Added floyd_warshall.py code algorithm in python

---
+++ Python/floyd_warshall.py
@@ -0,0 +1,42 @@
+
+V = 3
+
+
+INF = 86757
+
+
+
+def floydWarshall(graph):
+
+	dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
+
+	for k in range(V):
+
+		for i in range(V):
+
+			
+			for j in range(V):
+
+				dist[i][j] = min(dist[i][j],
+								dist[i][k] + dist[k][j]
+								)
+	printSolution(dist)
+
+
+def printSolution(dist):
+	print("Following matrix shows the shortest distances between every pair of vertices")
+	for i in range(V):
+		for j in range(V):
+			if(dist[i][j] == INF):
+				print("%7s" % ("INF"), end=" ")
+			else:
+				print("%7d\t" % (dist[i][j]), end=' ')
+			if j == V-1:
+				print()
+if __name__ == "__main__":
+
+    graph = [[0, 5, INF, 10],
+		[INF, 0, 3, INF],
+		[INF, INF, 0, 1],
+		]
+floydWarshall(graph)
