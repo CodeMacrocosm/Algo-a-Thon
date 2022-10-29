bad4d51d2e74f45e38a4c7b998be5f9da48aa430
Dhruv Merchant <94761283+DhruvMerchant@users.noreply.github.com>
2022-10-30 00:14:48 +0530

    Create coinchange.java

---
+++ Java/coinchange.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+class coinchange {
+
+
+	static int count(int coins[], int n, int sum)
+	{
+
+	
+		if (sum == 0)
+			return 1;
+
+		
+		if (sum < 0)
+			return 0;
+
+		
+		if (n <= 0)
+			return 0;
+
+		return count(coins, n - 1, sum)
+			+ count(coins, n, sum - coins[n - 1]);
+	}
+
+	public static void main(String args[])
+	{
+		int coins[] = { 1, 2, 3 };
+		int n = coins.length;
+
+		System.out.println(count(coins, n, 4));
+	}
+}
+
+
